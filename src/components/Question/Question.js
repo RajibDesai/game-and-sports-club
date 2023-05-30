58f50a2e131c08b7d11111fab9af4fb5455a8d75
RajibDesai <rajib.desai@gmail.com>
2023-05-31 00:06:31 +0600

    second answer

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -12,6 +12,20 @@ const Question = () => {
             </div>
             <div>
                 <h4>props Vs. state</h4>
+                <p>The status of a react class component 
+                    instance can be described 
+                     as an object of a collection of observed features
+                      that control 
+                     the component’s behavior. In other words, the component’s 
+                    state is an entity with some details that can 
+                    alter during the component’s lifetime.</p>
+                    <p>
+                    React is a library focused on components that separate the user 
+                    interface into reusable little bits. These components have to 
+                    transmit (send data to each other) 
+                    in some cases, and the way to transmit data among components
+                     is through props. 
+                    </p>
             </div>
         </div>
     );
